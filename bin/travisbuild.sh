da3086bde9c2429078f18fb7377a53adeeefae3a
Mengjuei Hsieh <mhsieh@pacificbiosciences.com>
2017-06-27 11:07:04 -0700

    URL change in HDFGroup

--- bin/travisbuild.sh
+++ bin/travisbuild.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 export PATH=/usr/lib/jvm/java-8-oracle/jre/bin:$PATH
 test -e $HOME/distfiles/hdf5-1.8.13-linux-x86_64-shared.tar.gz \
-|| curl -s -L https://www.hdfgroup.org/ftp/HDF5/releases/hdf5-1.8.13/bin/linux-x86_64/hdf5-1.8.13-linux-x86_64-shared.tar.gz \
+|| curl -s -L https://www.hdfgroup.org/ftp/HDF5/releases/hdf5-1.8/hdf5-1.8.13/bin/linux-x86_64/hdf5-1.8.13-linux-x86_64-shared.tar.gz \
         -o $HOME/distfiles/hdf5-1.8.13-linux-x86_64-shared.tar.gz
 tar zxf $HOME/distfiles/hdf5-1.8.13-linux-x86_64-shared.tar.gz -C $HOME
 cat mk/travis.mk > settings.mk
